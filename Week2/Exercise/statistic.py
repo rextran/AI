daf7d91789c4e5838899955343a08b569ea5ed1e
rextran <huytran.bku@gmail.com>
2019-07-18 15:42:08 +0700

    lan 4

--- Week2/Exercise/statistic.py
+++ Week2/Exercise/statistic.py
@@ -18,10 +18,40 @@ def calc_median(n):
 
 def calc_mode(n):
 	c = Counter(n)
-	print(c)
-	mode = c.most_common(1)
-	print(mode)
-	return mode[0][0]
+	mode = c.most_common()
+	max_val = mode[0][1]
+	result = [mode[0][0]]
+	for i in range(1, len(mode) -1):
+		if(max_val == mode[i][1]):
+			result.append(mode[i][0])
+	return result
+
+def frequency_table(n):
+	table = Counter(n)
+	numbers_freq = table.most_common()
+	numbers_freq.sort()
+	print("Number\tFrequency")
+	for i in numbers_freq:
+		print('{0}\t\t{1}'.format(i[0], i[1]))
+
+def rangeOfData(n):
+	lowest = min(n)
+	highest = max(n)
+	ran = highest - lowest
+	print('Lowest: {0}\tHighest: {1}\tRange: {2}'.format(lowest, highest, ran))
+
+def calc_variance(n):
+	mean = calc_mean(n)
+	N = len(n)
+	diff = []
+	for num in n:
+		diff.append(num - mean)
+	square_diff = []
+	for d in diff:
+		square_diff.append(d**2)
+		sum_square_diff = sum(square_diff)
+		variance = sum_square_diff/N
+	return variance
 start = time.time()
 donations = [100, 20.1, 39, 8, 12, 23, 12, 1020, 123, 291]
 points = [7, 8, 9, 2, 10, 9, 9, 9, 9, 4, 5, 6, 6, 6, 1, 5, 6, 7, 8, 6, 1, 10]
@@ -31,5 +61,8 @@ mode_Val = calc_mode(points)
 print("Trung binh", mean_Val)
 print("Trung vi", meadian_Val)
 print("Mode", mode_Val)
-print("m", )
+print(frequency_table(points))
+rangeOfData(points)
+print("Variance", calc_variance(points))
+print("Standard Deviation", calc_variance(points)**0.5)
 print("Thoi gian", (time.time() - start)*1000)
